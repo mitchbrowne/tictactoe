b54a48a2d37a3509628d18eb6d5b4543a2433de9
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-02-28 08:52:44 +1100

    Cleaned and commented scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
   let $w = $(window);
-  let lastScrollTop = $w.scrollTop();
   let _y = 0;
 
+  // scroll feature for background sun and clouds
   $w.scroll(function(event) {
     // assign variables to scrollTop and window height expression
     let st = $w.scrollTop();
     let comeUp = (st / $w.height()-5.2)*5;
     let sunVertical = ((st / $w.height())*97.5)+5;
     let sunHorizontal = (-(st / $w.height()))+5;
-    lastScrollTop = st;
     $('#sun').css('top', `${sunVertical}vh`);
     $('#sun').css('left', `${sunHorizontal}vw`);
 
+    // show button container when it reaches player page
     if (comeUp > 0) {
       $('#button_container').css('bottom','3vh');
       $('#button_container').css('opacity',comeUp);
